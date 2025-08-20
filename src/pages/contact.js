330b5784501de52ec8c1f677aa5f80c6058ff85c
solitdio079 <solitdio079@gmail.com>
2025-08-20 17:16:12 +0300

    Add contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,125 +1,189 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import { Link } from "gatsby";
 
 //import Seo from "../components/Seo";
-import firstImg from "../images/about.png"
+import firstImg from "../images/about.png";
 import Seo from "../components/Seo";
 
 const Contact = () => {
   return (
     <>
-    <Layout>
-      <div className="bg-base-200 py-8">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 motion-opacity-in-0 motion-translate-y-in-100 motion-blur-in-md">
-          <div className="relative mx-auto mb-12 w-fit">
-            <h2 className="text-primary text-3xl font-bold md:text-3xl lg:text-6xl">
-              Contactez-nous
-            </h2>
-            <span className="from-secondary/80 to-secondary/10 absolute start-0 top-9 lg:top-14 h-1 w-full rounded-full bg-gradient-to-r"></span>
-          </div>
-
-          <div className="grid items-start gap-12 lg:grid-cols-2">
-            <img
-              src={firstImg}
-              alt="Contact illustration"
-              className=" rounded-none intersect:motion-preset-slide-left intersect:motion-delay-[100ms] intersect:motion-ease-spring-bouncier"
-            />
-
-            <div>
-            
-              <p className="text-base-content/80 mb-10 text-lg font-medium intersect:motion-preset-slide-right intersect:motion-delay-[300ms] intersect:motion-ease-spring-bouncier">
-              La production est respectueuse de l’environnement et des partenariats sont établis avec des fournisseurs de matières et des distributeurs d’œufs, restaurants et hôtels et femmes revendeuses de produits agricoles. 
-
+      <Layout>
+        <div className="bg-base-200 py-8">
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 motion-opacity-in-0 motion-translate-y-in-100 motion-blur-in-md">
+            <div className="flex justify-center items-center flex-col gap-5 mx-auto mb-12 w-fit sm:mb-16 lg:mb-24">
+              <span className="uppercase text-primary">
+                {" "}
+                Cherifien Agrobusiness
+              </span>
+              <h2 className="text-primary text-3xl font-bold md:text-4xl lg:text-5xl">
+                Contactez-nous
+              </h2>
+              <p className="text-base-content text-md md:text-lg lg:text-xl">
+                Notre équipe est prête à vous aider — contactez-nous pour en
+                savoir plus sur nos services et solutions.
               </p>
+            </div>
 
-              <div className="grid gap-6 md:grid-cols-2">
-                <div className="card shadow-none intersect:motion-preset-slide-left intersect:motion-delay-[400ms] intersect:motion-ease-spring-bouncier">
-                  <div className="card-body items-center gap-3">
-                    <div className="avatar avatar-placeholder">
-                      <div className="border-primary/20 text-primary w-9 rounded-full border">
-                        <span className="icon-[tabler--clock] text-primary size-6"></span>
-                      </div>
-                    </div>
-                    <h4 className="text-base-content text-lg font-medium">
-                    Horaires 
-                    </h4>
-                    <div className="text-center">
-                      <p className="text-base-content/80">7j/7</p>
-                      <p className="text-base-content/80">24h/24</p>
-                    </div>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 sm:px-6 lg:px-8 my-12 mx-auto">
+              <div className="card card-border shadow-none ">
+                <div className="p-5 gap-5 flex flex-row">
+                  <Link
+                    to={"#"}
+                    className="w-fit p-3 bg-primary rounded-md mb-4"
+                  >
+                    <span className="icon-[tabler--send] text-primary-content size-8 rtl:rotate-180 "></span>
+                  </Link>
+
+                  <div>
+                    <h5 className="card-title mb-2.5 text-xl">Email</h5>
+                    <p className="mb-4">cherifien@cherifien.com</p>
                   </div>
                 </div>
+              </div>
+              <div className="card card-border shadow-none">
+                <div className="p-5 gap-5 flex flex-row">
+                  <Link
+                    to={"#"}
+                    className="w-fit p-3 bg-primary rounded-md mb-4"
+                  >
+                    <span className="icon-[tabler--phone] text-primary-content size-8 rtl:rotate-180 "></span>
+                  </Link>
 
-                <div className="card shadow-none intersect:motion-preset-slide-left intersect:motion-delay-[600ms] intersect:motion-ease-spring-bouncier">
-                  <div className="card-body items-center gap-3">
-                    <div className="avatar avatar-placeholder">
-                      <div className="border-primary/20 text-primary w-9 rounded-full border">
-                        <span className="icon-[tabler--map-pin] text-primary size-6"></span>
-                      </div>
-                    </div>
-                    <h4 className="text-base-content text-lg font-medium">
-                      Adresse
-                    </h4>
-                    <address className="text-base-content/80 text-center not-italic">
-                    Bamako – Mali
-                     
-                    </address>
+                  <div>
+                    <h5 className="card-title mb-2.5 text-xl">Telephone</h5>
+                    <p className="mb-4">+223 66 79 89 86</p>
                   </div>
                 </div>
+              </div>
+              <div className="card card-border shadow-none">
+                <div className="p-5 gap-5 flex flex-row">
+                  <Link
+                    to={"#"}
+                    className="w-fit p-3 bg-primary rounded-md mb-4"
+                  >
+                    <span className="icon-[tabler--map-pin] text-primary-content size-8 rtl:rotate-180 "></span>
+                  </Link>
 
-                <div className="card shadow-none intersect:motion-preset-slide-left intersect:motion-delay-[800ms] intersect:motion-ease-spring-bouncier">
-                  <div className="card-body items-center gap-3">
-                    <div className="avatar avatar-placeholder">
-                      <div className="border-primary/20 text-primary w-9 rounded-full border">
-                        <span className="icon-[tabler--briefcase] text-primary size-6"></span>
-                      </div>
-                    </div>
-                    <h4 className="text-base-content text-lg font-medium">
-                      Email
-                    </h4>
-                    <address className="text-base-content/80 text-center not-italic">
-                    cherifien@cherifien.com
-                     
-                    </address>
-                  </div>
-                </div>
-
-                <div className="card shadow-none intersect:motion-preset-slide-left intersect:motion-delay-[1000ms] intersect:motion-ease-spring-bouncier">
-                  <div className="card-body items-center gap-3">
-                    <div className="avatar avatar-placeholder">
-                      <div className="border-primary/20 text-primary w-9 rounded-full border">
-                        <span className="icon-[tabler--phone] text-primary size-6"></span>
-                      </div>
-                    </div>
-                    <h4 className="text-base-content text-lg font-medium">
-                      Telephone
-                    </h4>
-                    <div className="text-center">
-                      <p className="text-base-content/80">+223 66798986 </p>
-                     
-                    </div>
+                  <div>
+                    <h5 className="card-title mb-2.5 text-xl">Adresse</h5>
+                    <p className="mb-4">Bamamko- Mali</p>
                   </div>
                 </div>
               </div>
             </div>
+
+            <div className="grid md:grid-cols-3">
+              <div
+                className=" md:block md:col-span-2"
+                style={{ width: "100%" }}
+              >
+                <iframe
+                  width="100%"
+                  className="h-80 md:h-[650px] rounded-lg"
+                  src="https://maps.google.com/maps?width=100%25&amp;height=100%25&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+                >
+                  <a href="https://www.mapsdirections.info/fr/calculer-la-population-sur-une-carte">
+                    Carte démographique
+                  </a>
+                </iframe>
+              </div>
+
+              <div className="md:-ml-24 self-center">
+                <form
+                  name="contact"
+                  method="POST"
+                  data-netlify="true"
+                  netlify-honeypot="bot-field"
+                  className="p-5 shadow-md grid gap-6 sm:max-w-full grid-cols-1 bg-base-200 rounded-lg"
+                >
+                  <div className="flex justify-center items-center flex-col gap-5 mx-auto mb-12 w-fit sm:mb-8 lg:mb-12">
+                    <h2 className="text-base-content text-lg font-bold md:text-xl lg:text-2xl">
+                      Envoyez-nous un message
+                    </h2>
+                    <p className="text-base-content text-center text-md">
+                      Contactez-nous pour toute demande ou assistance. Nous
+                      sommes là pour vous aider et veiller à ce que votre
+                      expérience soit exceptionnelle.
+                    </p>
+                  </div>
+                  {/* This is a hidden input Netlify needs */}
+                  <input type="hidden" name="form-name" value="contact" />
+                  <p hidden>
+                    <label>
+                      Don’t fill this out if you’re human:{" "}
+                      <input name="bot-field" />
+                    </label>
+                  </p>
+                  <div className="input">
+                    <div className="input-floating grow">
+                      <input
+                        type="text"
+                        className="grow"
+                        placeholder="Djoko Keita"
+                        id="fullName"
+                      />
+                      <label
+                        className="input-floating-label ms-0"
+                        htmlFor="fullName"
+                      >
+                        Nom Complet
+                      </label>
+                    </div>
+                    <span className="icon-[tabler--user] text-base-content/80 my-auto ms-3 size-5 shrink-0"></span>
+                  </div>
+
+                  <div className="input">
+                    <div className="input-floating grow">
+                      <input
+                        type="email"
+                        className="grow"
+                        placeholder="abc@gmail.com"
+                        id="email"
+                      />
+                      <label
+                        className="input-floating-label ms-0"
+                        htmlFor="email"
+                      >
+                        Email
+                      </label>
+                    </div>
+                    <span className="icon-[tabler--mail] text-base-content/80 my-auto ms-3 size-5 shrink-0"></span>
+                  </div>
+                  <div className="textarea">
+                    <div className="textarea-floating grow">
+                      <textarea
+                        className="resize-none"
+                        placeholder="Hello!!!"
+                        id="textareaFloatingMedium"
+                      ></textarea>
+                      <label
+                        className="textarea-floating-label"
+                        htmlFor="textareaFloatingMedium"
+                      >
+                        Message
+                      </label>
+                    </div>
+                    <span className="icon-[tabler--message] text-base-content/80 mt-2 mx-4 size-5 shrink-0"></span>
+                  </div>
+
+                  <button
+                    type="button"
+                    className="btn btn-primary btn-next"
+                    data-stepper-next-btn=""
+                  >
+                    <span className="">Envoyer</span>
+                  </button>
+                  <p className="my-3 text-center">
+                    Je comprends que mes données seront conservées en toute
+                    sécurité conformément à la politique de confidentialité.
+                  </p>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="grid items-center gap-12 lg:grid-cols-1">
-        <div>
-          <div style={{ width: "100%" }}>
-            <iframe
-              width="100%"
-              className="h-80 lg:h-[350px]"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3892.911917048306!2d-7.9344892248710215!3d12.653779887633114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xe51d3004d077799%3A0x5714d50eeae0f5dc!2sRond-point%20G%C3%A9n%C3%A9ral%20Abdoulaye%20Soumar%C3%A9!5e0!3m2!1sen!2str!4v1754921257508!5m2!1sen!2str"
-            >
-              <a href="https://www.mapsdirections.info/fr/calculer-la-population-sur-une-carte">
-                Carte démographique
-              </a>
-            </iframe>
-          </div>
-        </div>
-      </div>
       </Layout>
     </>
   );
@@ -127,4 +191,4 @@ const Contact = () => {
 
 export default Contact;
 
-export const Head = () => <Seo title={"Contact"}/>;;
+export const Head = () => <Seo title={"Contact"} />;
