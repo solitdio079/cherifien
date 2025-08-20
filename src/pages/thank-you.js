579a6d73a400f00124475fb7ca93829fa189474b
solitdio079 <solitdio079@gmail.com>
2025-08-20 17:32:51 +0300

    Add layout to thank you page

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -7,7 +7,7 @@ import Layout from "../components/Layout";
 export default function ThankYouPage() {
   return (
     <Layout>
-      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-6">
+      <main className="flex flex-col bg-base-200 items-center justify-center min-h-96 p-6">
         <div className="bg-white shadow-lg rounded-2xl p-8 max-w-md text-center">
           <h1 className="text-3xl font-bold text-green-600 mb-4">🎉 Merci !</h1>
           <p className="text-gray-700 mb-6">
@@ -16,7 +16,7 @@ export default function ThankYouPage() {
           </p>
           <Link
             to="/"
-            className="bg-blue-600 text-white px-4 py-2 rounded-xl hover:bg-blue-700"
+            className="bg-primary text-white px-4 py-2 rounded-xl hover:bg-blue-700"
           >
             Retour à l’accueil
           </Link>
